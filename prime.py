cdb4e1ed80de82c6c3d1621ee0bc603bdfebef2a
shekh <shek>
2020-02-20 19:56:49 +0530

    Test commit

---
+++ prime.py
@@ -0,0 +1,4 @@
+
+Que.-Write a single line python code to print prime numbers between 50 and 150.
+please write the answers here only.
+[print(x) for x in range(50, 150) if not list(filter(lambda y: (x % y) == 0, range(2, x - 1)))]
